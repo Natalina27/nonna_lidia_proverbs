964bde5f3e492f64ddeb06f489ebe6bcce6d6471
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-02 14:54:16 +0200

    3-02 props-children

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import HeaderBlock from './Header/HeaderBlock';
+import HeaderBlock from './HeaderBlock/HeaderBlock';
 import Footer from './Footer/Footer';
 import s from './App.module.scss';
+import Header from "./HeaderBlock/Header/Header";
+import Paragraph from "./HeaderBlock/Paragraph/Paragraph";
 
 function App() {
     return (
         <div className={s.root}>
-            <HeaderBlock
-                title='PROVERBI DI NONNA LIDIA'
-                desc='I proverbi della mia nonna in dialetto di San Gregorio'
-            />
+            <HeaderBlock>
+                <Header>
+                    PROVERBI DI NONNA LIDIA
+                </Header>
+                <Paragraph>
+                    I proverbi della mia nonna in dialetto di San Gregorio
+                </Paragraph>
+            </HeaderBlock>
             <HeaderBlock
                 title='PROVERBI:'
                 hideBackground
--- src/components/App/HeaderBlock/HeaderBlock.js
+++ src/components/App/HeaderBlock/HeaderBlock.js
@@ -4,7 +4,8 @@ import {ReactComponent as ReactLogoSVg} from '../../../assets/logo.svg';
 
 console.log('###ReactLogoSVg: ', ReactLogoSVg);
 
-const HeaderBlock = ({title, desc, hideBackground = false}) => {
+const HeaderBlock = ({title, desc, hideBackground = false, children}) => {
+
     const styleCover = hideBackground ? { background: 'none'} : {};
     return (
         <div className={s.cover} style={styleCover}>
@@ -13,6 +14,7 @@ const HeaderBlock = ({title, desc, hideBackground = false}) => {
                 {/*<p className={s.description}>{desc}</p>*/}
                 {title && <h1 className={s.header}>{title}</h1>}
                 {desc && <p className={s.description}>{desc}</p>}
+                {children}
             </div>
         </div>
 
