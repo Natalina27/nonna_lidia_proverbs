466bcbb96e55d9e1b463d08d8893589fb64bfd7b
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-09 08:36:21 +0200

    Updates

---
+++ precache-manifest.7a9ce57f935253dd2ef41281005be78a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5f1ca224186f493e12dfaf37ea0165",
+    "url": "/nonna_lidia_proverbs/index.html"
+  },
+  {
+    "revision": "2e74e2e3244f90d34cb5",
+    "url": "/nonna_lidia_proverbs/static/css/main.7a820a41.chunk.css"
+  },
+  {
+    "revision": "7fd402c30f4a47e2c342",
+    "url": "/nonna_lidia_proverbs/static/js/2.0b692a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nonna_lidia_proverbs/static/js/2.0b692a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e74e2e3244f90d34cb5",
+    "url": "/nonna_lidia_proverbs/static/js/main.812c26ca.chunk.js"
+  },
+  {
+    "revision": "74522084244bee060863",
+    "url": "/nonna_lidia_proverbs/static/js/runtime-main.648fa088.js"
+  },
+  {
+    "revision": "dab428e7719ca5b2341f7ab93f99408a",
+    "url": "/nonna_lidia_proverbs/static/media/background-body.dab428e7.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nonna_lidia_proverbs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "520a4a2efa539d90a38526ca1f2da7fb",
+    "url": "/nonna_lidia_proverbs/static/media/nonnaeSophie.520a4a2e.jpeg"
+  }
+]);
