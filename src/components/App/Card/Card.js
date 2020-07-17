6a53a01751275b48af4fea8a7caaf1cb1d84622b
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-17 08:15:23 +0200

    flip card by click (added done class)

--- src/components/App/Card/Card.js
+++ src/components/App/Card/Card.js
@@ -1,30 +1,42 @@
 import React from 'react';
 import s from './Card.module.scss';
 
-class Card extends React.Component  {
+class Card extends React.Component {
 
-    // let count = 1;
-    // setInterval(() => {
-    //         count++;
-    //         console.log('#count', count);
-    //     }, 1000);
-    //
-render() {
-    const { prov, name } = this.props;
-    return (
-            <div className={s.card}>
+    state = {
+        done: false
+    };
+
+    handleCardClick = () => {
+        this.setState({
+            done: !this.state.done
+        })
+    };
+
+    render() {
+        const cardClass = [s.card];
+        const { done } = this.state;
+        const {prov, name} = this.props;
+
+        if (done) {
+            cardClass.push(s.done);
+        }
+        return (
+            <div className={cardClass.join(' ')}
+                 onClick={this.handleCardClick}
+            >
                 <div className={s.cardInner}>
                     <div className={s.cardFront}>
-                        { prov }
+                        {prov}
                         {/*, { count }*/}
                     </div>
                     <div className={s.cardBack}>
-                        { name }
+                        {name}
                     </div>
                 </div>
             </div>
-    );
-};
+        );
+    };
 }
 
 export default Card;
